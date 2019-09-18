14f013575431978fe06f2556d185a705873942d7
unknown <munnyshaik77@gmail.com>
2019-09-18 22:30:45 +0530

    second commit

--- time.js
+++ time.js
@@ -11,12 +11,11 @@ $(document).ready(function () {
         var target = event.target;
         var targetId = target.id;
         if (targetId === "next_day" || targetId === "next_day_a") {
-
-            ShowLocalDate();
+            nextDay();
         }
         else if (targetId === "next_week" || targetId === "next_week_a") {
             console.log("next_week");
-
+            nextWeek();
         }
         else if (targetId === "next_month" || targetId === "next_month_a") {
             next_month();
@@ -27,26 +26,33 @@ $(document).ready(function () {
     })
 });
 
-function ShowLocalDate() {
-    var dNow = new Date();
-    var localdate = (dNow.getMonth() + 1) + '/' + dNow.getDate() + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
-    $('#currentDate').text(localdate)
+function nextDay() {
+    var CurrentDate = new Date();
+    CurrentDate.setDate(CurrentDate.getDate() + 1)
+    var localdate = (CurrentDate.getMonth() + 1) + '/' + CurrentDate.getDate() + '/' + CurrentDate.getFullYear() + ' ' + CurrentDate.getHours() + ':' + CurrentDate.getMinutes();
+    $('#currentDate').text("nextDay:" + localdate);
+}
+
+function nextWeek() {
+    var CurrentDate = new Date();
+    CurrentDate.setDate(CurrentDate.getDate() + 7);
+    var localdate = (CurrentDate.getMonth() + 1) + '/' + CurrentDate.getDate() + '/' + CurrentDate.getFullYear() + ' ' + CurrentDate.getHours() + ':' + CurrentDate.getMinutes();
+    $('#currentDate').text("nextWeek:" + localdate);
+}
+
+
+
+function next_month() {
+    var CurrentDate = new Date();
+    console.log("Current date:", CurrentDate);
+    CurrentDate.setMonth(CurrentDate.getMonth() + 1);
+    var localdate = (CurrentDate.getMonth() + 1) + '/' + CurrentDate.getDate() + '/' + CurrentDate.getFullYear() + ' ' + CurrentDate.getHours() + ':' + CurrentDate.getMinutes();
+    $('#currentDate').text("next_month:" + localdate);
 }
 
 function next_threeMonth() {
-    var x = 3; //or whatever offset
     var CurrentDate = new Date();
-    CurrentDate.setMonth(CurrentDate.getMonth() + x);
+    CurrentDate.setMonth(CurrentDate.getMonth() + 3);
     var localdate = (CurrentDate.getMonth() + 1) + '/' + CurrentDate.getDate() + '/' + CurrentDate.getFullYear() + ' ' + CurrentDate.getHours() + ':' + CurrentDate.getMinutes();
-    $('#currentDate').text(localdate);
+    $('#currentDate').text("next_threeMonth:" + localdate);
 }
-
-function next_month() {
-    var x = 1; //or whatever offset
-    var CurrentDate = new Date();
-    console.log("Current date:", CurrentDate);
-    CurrentDate.setMonth(CurrentDate.getMonth() + x);
-
-    var localdate = (CurrentDate.getMonth() + 1) + '/' + CurrentDate.getDate() + '/' + CurrentDate.getFullYear() + ' ' + CurrentDate.getHours() + ':' + CurrentDate.getMinutes();
-    $('#currentDate').text(localdate);
-} 
